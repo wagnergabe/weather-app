d462f993a9bf1900507fa8933ffbc818412dca71
GSvenny <gabrielswagner7@gmail.com>
2022-06-03 17:02:50 -0500

    added API fetch/awaiting API key verification

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,18 @@
 /* Element variables*/
 var searchBtnEl = document.getElementById('searchBtn');
 
+
+/*API Fetch*/ 
+
 searchBtnEl.addEventListener("click", getCities) 
 function getCities() {
-    alert("test")
-}
-/*API Fetch*/ 
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=649cad07f48dfced963391132fc3e4be"
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+};
