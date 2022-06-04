3841adb09634772b2a14c325a46bfa0a11042042
GSvenny <gabrielswagner7@gmail.com>
2022-06-03 22:35:11 -0500

    added locations via city search

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 /* Element variables*/
 var searchBtnEl = document.getElementById('searchBtn');
 var cardEl = document.getElementById('card')
-
+var apiKey= "649cad07f48dfced963391132fc3e4be"
 
 
 /*API Fetch*/ 
@@ -11,9 +11,9 @@ function getCities() {
     /*Chicago test*/ 
     /*Need to provide city name, date, icon for weather cond. */
     /*Temperature (temp), Humidity (humidity), wind speed(wind_speed), UV Index(uvi)*/ 
-
-    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=649cad07f48dfced963391132fc3e4be"
-    fetch(url)
+    var cityName = document.getElementById('citySearch').value;
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=649cad07f48dfced963391132fc3e4be&units=imperial`
+        fetch(url)
         .then(function (response) {
             return response.json();
         })
@@ -22,20 +22,20 @@ function getCities() {
             console.log(data);
 
             var temperature = document.createElement('li');
-            temperature.textContent = data.current.temp;
+            temperature.textContent ="Temperature: " + data.main.temp + " degrees F";
             card.appendChild(temperature);
 
             var humidity = document.createElement('li');
-            humidity.textContent = "Humidity: " + data.current.humidity + "%";
+            humidity.textContent = "Humidity: " + data.main.humidity + "%";
             card.appendChild(humidity);
 
             var windSpeed = document.createElement('li');
-            windSpeed.textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
+            windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH";
             card.appendChild(windSpeed);
 
-            var uvIndex = document.createElement('li');
-            uvIndex.textContent = "UV Index: " + data.current.uvi;
-            card.appendChild(uvIndex);
+            // var uvIndex = document.createElement('li');
+            // uvIndex.textContent = "UV Index: " + data.current.uvi;
+            // card.appendChild(uvIndex);
             
 
         })
