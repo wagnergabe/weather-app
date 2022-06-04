a8c21d090a9a814e724dd9ffa3f86294ee1315ed
GSvenny <gabrielswagner7@gmail.com>
2022-06-03 20:53:00 -0500

    temp data display

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,31 @@
 /* Element variables*/
 var searchBtnEl = document.getElementById('searchBtn');
+var cardEl = document.getElementById('card')
+
 
 
 /*API Fetch*/ 
 
-searchBtnEl.addEventListener("click", getCities) 
+
 function getCities() {
-    var APIkey = "649cad07f48dfced963391132fc3e4be"
     /*Chicago test*/ 
+    /*Need to provide city name, date, icon for weather cond. */
+    /*Temperature (temp), Humidity (humidity), wind speed(wind_speed), UV Index(uvi)*/ 
+
     var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=649cad07f48dfced963391132fc3e4be"
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
+
+            console.log(data);
+
+            var temperature = document.createElement('li');
+            temperature.textContent = data.current.temp;
+            card.appendChild(temperature);
+            
+
         })
 };
+searchBtnEl.addEventListener("click", getCities) 
