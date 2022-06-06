fb24d9909e80fd33b895f9c6fbd3f44ff9856596
GSvenny <gabrielswagner7@gmail.com>
2022-06-05 21:23:03 -0500

    finially figured out UV Index call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,13 +8,13 @@ var mainCardEl= document.getElementById('weatherStats')
 /*API Fetch*/ 
 
 
-var getCities = function() {
+function getCities() {
     
     /*Need to provide city name, date, icon for weather cond. */
     /*Temperature (temp), Humidity (humidity), wind speed(wind_speed), UV Index(uvi)*/ 
     /*Onecall api having trouble finding city*/ 
     var cityName = document.getElementById('citySearch').value;
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=649cad07f48dfced963391132fc3e4be&units=imperial`
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 
         fetch(url)
         .then(function (response) {
@@ -35,9 +35,16 @@ var getCities = function() {
             var windSpeed = document.createElement('li');
             windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH";
             mainCardEl.appendChild(windSpeed);
-            
 
-            fetch (`https://api.openweathermap.org/data/3.0/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=649cad07f48dfced963391132fc3e4be`)
+            var lon = data.coord.lon;
+            var lat = data.coord.lat;
+
+            console.log(lon, lat);
+
+            /*Ran into a little bit of a paradox with the "onecall" and "weather" calls. "onecall" needs city geolocation(lat and lon) while "weather" didn't supply UV index */
+            /* Seems like a contrived approach, but two calls to the api (one for the main temp data, and city location) followed by a call entering said "lat and lon" eventually worked*/
+            var uviURL= `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+                fetch(uviURL)
             .then(function (response) {
                 return response.json();
             })
@@ -46,7 +53,7 @@ var getCities = function() {
                 console.log(data)
                 
                 var uvi = document.createElement('li');
-                uvi.textContent = data.uvi;
+                uvi.textContent = "UV Index: " + data.current.uvi;
                 mainCardEl.appendChild(uvi);
             })
             
